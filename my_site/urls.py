08f54963af40dd014bf75e1915ae7a9e51c35715
Gabrielly Bernardelli <gabrielly.bernardelli@aluno.ifsp.edu.br>
2023-05-09 10:58:24 -0300

    #

--- my_site/urls.py
+++ my_site/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index')
+    path('sobre/', views.sobre, name='sobre' ),
 ]
+
+
+
+
+
