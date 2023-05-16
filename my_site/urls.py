fad842e6f972467b51a431e4323958b52b98479d
gabybernardelli <gabrielly.bernardelli@aluno.ifsp.edu.br>
2023-05-16 11:16:11 -0300

    #

--- my_site/urls.py
+++ my_site/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre' ),
 ]
 
